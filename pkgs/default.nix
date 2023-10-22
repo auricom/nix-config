cd9a9996bd40e180c7ff20037ea8368b31016740
auricom <27022259+auricom@users.noreply.github.com>
2023-10-22 10:05:47 +0200

    wip

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,8 @@
 {pkgs}:
 pkgs.lib.makeScope pkgs.newScope (pkgs: {
+  comictagger = pkgs.callPackage ./comictagger {};
+
   ff2mpv-go = pkgs.callPackage ./ff2mpv-go {};
+
+  kopia-ui = pkgs.callPackage ./kopia-ui {};
 })
