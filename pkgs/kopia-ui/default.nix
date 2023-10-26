7a0e9c193611fa59062c90bee5fb9d3ad91cf7f5
auricom <27022259+auricom@users.noreply.github.com>
2023-10-26 22:50:32 +0200

    wip

--- pkgs/kopia-ui/default.nix
+++ pkgs/kopia-ui/default.nix
@@ -3,13 +3,13 @@
   pkgs,
 }: let
   pname = "kopia-ui";
-  version = "v0.15.0"; # renovate: datasource=github-tags depName=kopia/kopia versioning=semver nix=fetchurl
+  version = "v0.14.1"; # renovate: datasource=github-tags depName=kopia/kopia versioning=semver nix=fetchurl
   version_mod = builtins.replaceStrings ["v"] [""] version;
   name = "${pname}-${version}";
 
   src = pkgs.fetchurl {
     url = "https://github.com/kopia/kopia/releases/download/${version}/KopiaUI-${version_mod}.AppImage";
-    hash = "sha256-Q7NXxnDrOXTtALx8vPanrUugW2AnXJifvto5phehOFQ="; # renovate: datasource=github-tags depName=kopia/kopia
+    hash = "fff"; # renovate: datasource=github-tags depName=kopia/kopia
   };
 
   appimageContents = pkgs.appimageTools.extractType2 {inherit name src;};
