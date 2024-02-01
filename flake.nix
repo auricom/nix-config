cd079852ef0cb4974b0b185b4db5523743e45645
auricom <27022259+auricom@users.noreply.github.com>
2024-02-01 18:06:25 +0100

    🔧 nixpkgs stable

--- flake.nix
+++ flake.nix
@@ -36,17 +36,21 @@
       type = "github";
       owner = "nix-community";
       repo = "home-manager";
-      ref = "master";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      # ref = "nixos-unstable";
+      ref = "release-23.11";
+
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs dependencies.
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixpkgs = {
       type = "github";
       owner = "NixOS";
       repo = "nixpkgs";
-      ref = "nixos-unstable";
+      # ref = "nixos-unstable";
+      ref = "nixos-23.11";
     };
 
     nur = {
