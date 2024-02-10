c24925140c888949c671343ac5c211091c529da3
auricom <27022259+auricom@users.noreply.github.com>
2024-02-10 16:58:55 +0100

    🔧 vscodium unstable

--- home/desktop/development/vscodium.nix
+++ home/desktop/development/vscodium.nix
@@ -1,7 +1,7 @@
 {
   config,
   lib,
-  pkgs,
+  inputs,
   ...
 }: let
   cfg = config.my.home.development;
@@ -9,7 +9,7 @@ in {
   config = lib.mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      package = pkgs.vscodium;
+      package = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.vscodium;
     };
   };
 }
